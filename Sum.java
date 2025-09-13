822306cc54fccd7e584a3f003db7bc50feb7feca
rushi4133 <159619018+rushi4133@users.noreply.github.com>
2025-09-13 08:25:59 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Sum{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println( sum);
+    }
+
+    {
+ 
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println(input);
+    }
+}
